feba7967b1513317f4e227c6ea68c3b4385400dd
devband777 <world.devband777@gmail.com>
2023-05-13 05:12:56 +0300

    insert keyboard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "./style.css";
 import { useNavigate } from "react-router-dom";
@@ -8,10 +8,76 @@ import { HttpURL, Api, SelectedDataHttpURL } from "../utils.js";
 
 const Home = () => {
   const [values, setValues] = useState({});
-
+  const inputRefs = useRef([]);
+  const submitRef = useRef(null);
   const [jsonData, setJsonData] = useState({ items: {} });
+  const [idx, setIdx] = useState(0);
 
   const navigate = useNavigate();
+
+  const handleKeyDown = (e, index) => {
+    
+    const keyCode = e.keyCode || e.which;
+    if (keyCode < 49 || keyCode > 53) {
+      e.preventDefault();
+      return;
+    }
+    const num = String.fromCharCode(keyCode);
+    setValues({ ...values, [index]: num });
+    let keyboardItems = document.getElementsByClassName('keyboard-item');
+    // console.log(num);
+    keyboardItems[num-1].setAttribute("disabled", true);
+    if (index === Object.keys(jsonData.items).length - 1) {
+      submitRef.current.focus();
+      inputRefs.current[index].classList.remove("active");
+      inputRefs.current[index].setAttribute("contentEditable", false);
+    } else {
+      const nextInput = inputRefs.current[index + 1];
+      if (nextInput) {
+        inputRefs.current[index].setAttribute("contentEditable", false);
+        inputRefs.current[index].classList.remove("active");
+        nextInput.classList.add("active");
+        nextInput.setAttribute("contentEditable", true);
+        nextInput.focus();
+      }
+    }
+  };
+
+  const handleKeyBoardPress = (e) => {
+    e.preventDefault();
+    setValues({ ...values, [idx]: e.target.value });
+    
+    e.target.setAttribute("disabled", true)
+    if (idx === Object.keys(jsonData.items).length - 1) {
+      submitRef.current.focus();
+      inputRefs.current[idx].classList.remove("active");
+      inputRefs.current[idx].setAttribute("contentEditable", false);
+      return;
+    } 
+      const nextInput = inputRefs.current[idx + 1];
+      if (nextInput) {
+        inputRefs.current[idx].setAttribute("contentEditable", false);
+        inputRefs.current[idx].classList.remove("active");
+        nextInput.classList.add("active");
+        nextInput.setAttribute("contentEditable", true);
+        nextInput.focus();
+      }
+      setIdx(idx+1);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    const updatedItems = jsonData.items;
+    Object.keys(jsonData.items).forEach((item, index) => {
+      const newValue = values[index] || 0;
+      updatedItems[item].rank = jsonData.items[item].rank + parseInt(newValue);
+    });
+    Api.put(SelectedDataHttpURL, { ...jsonData, items: updatedItems })
+      .then((res) => console.log("Updated: ", res.data.record))
+      .catch((err) => console.log("Error: ", err));
+    navigate("/chart");
+  };
 
   useEffect(() => {
     Api.get(HttpURL)
@@ -28,29 +94,14 @@ const Home = () => {
         console.log("Data fetched:", res.data.record);
       })
       .catch((error) => console.log("Error saving data:", error));
+
   }, []);
 
-  const handleChange = (e, key) => {
-    const newValue = e.target.value;
-    if (/^\d+$/.test(newValue) && newValue >= 1 && newValue <= 5) {
-      setValues({ ...values, [key]: newValue });
-    } else {
-      setValues({ ...values, [key]: 0 });
-    }
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const updatedItems = jsonData.items;
-    Object.keys(jsonData.items).forEach((item) => {
-      const newValue = values[item] || 0;
-      updatedItems[item].rank = jsonData.items[item].rank + parseInt(newValue);
-    });
-    console.log(updatedItems);
-    Api.put(SelectedDataHttpURL, { ...jsonData, items: updatedItems })
-      .then((res) => console.log("Updated: ", res.data.record))
-      .catch((err) => console.log("Error: ", err));
-    navigate("/chart");
-  };
+  useEffect(()=> {
+    inputRefs.current[0]?.setAttribute("contentEditable", true);
+    inputRefs.current[0]?.classList.add("active");
+  }, [jsonData])
+  
   return (
     <section>
       <Container fluid className="home-container">
@@ -67,7 +118,7 @@ const Home = () => {
             </Row>
             {jsonData.items ? (
               <Row className="foods mb-5">
-                {Object.keys(jsonData.items).map((item) => (
+                {Object.keys(jsonData.items).map((item, index) => (
                   <div
                     className="d-flex flex-direction-row justify-content-between"
                     key={item}
@@ -78,11 +129,12 @@ const Home = () => {
                       </div>
                     </Col>
                     <Col xl={2} md={2} sm={2} xs={2}>
-                      <input
-                        type="text"
-                        value={values[item] || ""}
-                        onChange={(e) => handleChange(e, item)}
-                        className="form-control col-md-4 col-sm-4"
+                      <div
+                        className="input-num"
+                        contentEditable={false}
+                        onKeyDown={(e) => handleKeyDown(e, index)}
+                        ref={(el) => (inputRefs.current[index] = el)}
+                        dangerouslySetInnerHTML={{ __html: values[index] }}
                       />
                     </Col>
                   </div>
@@ -91,9 +143,17 @@ const Home = () => {
             ) : (
               <p className="loading">Loading...</p>
             )}
+            <Row className="d-flex justify-content-evenly mb-5 keyboard">
+              <button className="keyboard-item" onClick={(e) => handleKeyBoardPress(e)} value={1}>1</button >
+              <button className="keyboard-item" onClick={(e) => handleKeyBoardPress(e)} value={2}>2</button >
+              <button className="keyboard-item" onClick={(e) => handleKeyBoardPress(e)} value={3}>3</button >
+              <button className="keyboard-item" onClick={(e) => handleKeyBoardPress(e)} value={4}>4</button >
+              <button className="keyboard-item" onClick={(e) => handleKeyBoardPress(e)} value={5}>5</button >
+            </Row>
             <Row>
               <Col md={6} xl={6}>
                 <input
+                  ref={submitRef}
                   type="submit"
                   className="view-chart"
                   value="View Chart"
