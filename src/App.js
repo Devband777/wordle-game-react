2abed055b0d3be6898cd8e1f4cd5b3607797b2fe
devband777 <world.devband777@gmail.com>
2023-05-17 21:11:17 +0300

    update show result method

--- src/App.js
+++ src/App.js
@@ -12,22 +12,12 @@ import MyChart from "./pages/MyChart";
 import "./App.css";
 import "./style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import initData from "./data.json";
-import { Api, HttpURL } from "./utils.js";
 
 function App() {
-  const [load, upadateLoad] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      upadateLoad(false);
-    }, 1200);
-    return () => clearTimeout(timer);
-  }, []);
 
   return (
     <Router>
-      <div className="App" id={load ? "no-scroll" : "scroll"}>
+      <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route exact path="/:topicTitle" element={<Home />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,93 +1,70 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Container, Row } from "react-bootstrap";
 import "./style.css";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { HttpURL, Api } from "../utils.js";
+import { Modal, Button } from "react-bootstrap";
+import ShareButton from "react-social-share-buttons";
+import initialData from "../data.json";
 
 const Home = () => {
-  const [values, setValues] = useState([]);
   const inputRefs = useRef([]);
-  const submitRef = useRef(null);
-  const [jsonData, setJsonData] = useState({ items: {} });
+  const [todayTopicData, setTodayTopicData] = useState({
+    topic: "",
+    items: [],
+    values: [],
+  });
   const [keyboardIdx, setKeyboardIdx] = useState(0);
 
   const [isBtnClickable, setIsBtnClickable] = useState(true);
-  const [count, setCount] = useState(90);
+  const [count, setCount] = useState(90.0);
   const [visibleItems, setVisibleItems] = useState(1);
 
-  const navigate = useNavigate();
+  const { topicTitleParam } = useParams();
 
-  const { topicTitle } = useParams();
+  const [modalShow, setModalShow] = useState(false);
+  const handleClose = () => setModalShow(false);
 
-  const getNumFromKeyCode = (keyCode) => {
-    switch (keyCode) {
-      case 97:
-      case 49:
-        return 1;
-      case 98:
-      case 50:
-        return 2;
-      case 99:
-      case 51:
-        return 3;
-      case 100:
-      case 52:
-        return 4;
-      case 101:
-      case 53:
-        return 5;
-      default:
-        return null;
+  //get the radom records from array
+  const getRandomRecords = (arr, num) => {
+    const result = [];
+    for (let i = 0; i < num; i++) {
+      const randomIndex = Math.floor(Math.random() * arr.length);
+      result.push(arr[randomIndex]);
+      arr.splice(randomIndex, 1);
     }
-  };
-
-  const handleKeyDown = (e, index) => {
-    const keyCode = e.keyCode || e.which;
-    const num = getNumFromKeyCode(keyCode);
-
-    if (num == null || values.includes(num)) {
-      e.preventDefault();
-      return;
-    }
-
-    setValues((prevValues) => [...prevValues, num]);
-    let keyboardItems = document.getElementsByClassName("keyboard-item");
-    keyboardItems[num - 1].setAttribute("disabled", true);
-    if (index === Object.keys(jsonData.items).length - 1) {
-      submitRef.current.focus();
-      inputRefs.current[index].classList.remove("active");
-      inputRefs.current[index].setAttribute("contentEditable", false);
-    } else {
-      const nextInput = inputRefs.current[index + 1];
-      if (nextInput) {
-        e.preventDefault();
-        inputRefs.current[index].setAttribute("contentEditable", false);
-        inputRefs.current[index].classList.remove("active");
-        nextInput.classList.add("active");
-        nextInput.setAttribute("contentEditable", true);
-        nextInput.focus();
-      }
-    }
-
-    setKeyboardIdx(keyboardIdx + 1);
+    return result;
   };
 
   const handleKeyBoardPress = (e) => {
     e.preventDefault();
     const num = parseInt(e.target.value);
 
-    if (num == null || values.includes(num)) {
+    if (num == null || todayTopicData.values.includes(num)) {
       return;
     }
 
     setVisibleItems((prevVisibleItems) => prevVisibleItems + 1);
-    setValues((prevValues) => [...prevValues, e.target.value]);
+    const newValue = e.target.value;
 
+    setTodayTopicData((prevData) => ({
+      ...prevData,
+      values: [...prevData.values, newValue],
+    }));
+
+    console.log(todayTopicData);
+
+    e.target.setAttribute("disabled", true);
+
+    setKeyboardIdx(keyboardIdx + 1);
     e.target.setAttribute("disabled", true);
 
     setKeyboardIdx(keyboardIdx + 1);
   };
 
+
+  const [rankedItems, setRankedItems] = useState([]);
+  //confrom today
   useEffect(() => {
     // Get today's date in ISO format without the time component
     const today = new Date().toISOString().substring(0, 10);
@@ -119,102 +96,88 @@ const Home = () => {
   useEffect(() => {
     let timerId;
     if (isBtnClickable) {
+      let time = 0;
+
       timerId = setInterval(() => {
         if (count > 0) {
-          setCount((count) => count - 1);
+          setCount((count) => count - 0.1);
         }
-      }, 1000);
+      }, 100);
     }
     return () => clearInterval(timerId);
-  }, [isBtnClickable, count]); // add count as a dependency
+  }, [count]); // add count as a dependency
 
   useEffect(() => {
-    Api.get(HttpURL)
-      .then((res) => {
-        // Get an array of topic values from the object
-        const topicValues = Object.values(res.data.record);
 
-        if (topicTitle) {
-          const getedTopic = Object.keys(topicValues).find(
-            (key) => topicValues[key].title === topicTitle
-          );
-          setJsonData(topicValues[getedTopic]);
+    if (topicTitleParam) {
+          setTodayTopicData({
+            ...todayTopicData,
+            topic: topicTitleParam,
+            items: getRandomRecords(initialData[topicTitleParam], 5),
+          });
         } else {
-          // Get a random index in the array
-          const randomIndex = Math.floor(Math.random() * topicValues.length);
-          // Use the random index to get a random topic from the array
-          const randomTopic = topicValues[randomIndex];
-          setJsonData(randomTopic);
+          const topics = Object.keys(initialData);
+          const randomTopic = topics[Math.floor(Math.random() * topics.length)];
+          setTodayTopicData({
+            ...todayTopicData,
+            topic: randomTopic,
+            items: getRandomRecords(initialData[randomTopic], 5),
+          });
         }
+    
+    // return () => Api.get(HttpURL)
+    //   .then((res) => {
+        
 
-        console.log("Data fetched:", res.data.record);
-      })
-      .catch((error) => console.log("Error saving data:", error));
+    //     console.log("Data fetched:", initialData);
+    //   })
+    //   .catch((error) => console.log("Error saving data:", error));
   }, []);
 
   useEffect(() => {
     if (keyboardIdx === 5) {
-      const updatedItems = jsonData.items;
-      Object.keys(jsonData.items).forEach((item, index) => {
-        const newValue = values[index] || 0;
-        updatedItems[item].rank =
-          jsonData.items[item].rank + parseInt(newValue);
-      });
+      console.log(todayTopicData);
+
+      const temp = todayTopicData.items;
+
+      setRankedItems(temp.sort(
+        (a, b) =>
+          todayTopicData.values[todayTopicData.items.indexOf(a)] -
+          todayTopicData.values[todayTopicData.items.indexOf(b)]
+      ));
+
       const today = new Date().toISOString().substring(0, 10);
       localStorage.setItem("lastSubmitDate", today);
-      setIsBtnClickable(false);
-
-      Api.get(HttpURL)
-        .then((res) => {
-          // Get an array of topic values from the object
-          const currentData = Object.values(res.data.record);
-          const topicToUpdate = Object.keys(currentData).find(
-            (key) => currentData[key].title === jsonData.title
-          );
-          if (topicToUpdate) {
-            const updatedTopic = {
-              ...currentData[topicToUpdate],
-              items: jsonData.items,
-            };
-            const updatedJsonData = {
-              ...currentData,
-              [topicToUpdate]: updatedTopic,
-            };
-            Api.put(HttpURL, updatedJsonData)
-              .then((res) => {
-                navigate("/chart", { state: { passData: jsonData } });
-                console.log("Updated: ", res.data.record);
-              })
-              .catch((err) => console.log("Error: ", err));
-          }
-        })
-        .catch((error) => console.log("Error saving data:", error));
+      setIsBtnClickable(false); 
+      setModalShow(true);
     }
   }, [keyboardIdx]);
+
   return (
     <section>
       <Container fluid className="home-container">
         <div className="col-md-6 col-xxl-4 col-xl-4 col-sm-12 col-xs-12 offset-xl-4 offset-md-3 offset-xxl-4">
-          <Row className="header mb-5">
+          <Row className="header mb-3">
             <p>
               Maazle <b className="green">Game</b>{" "}
               <small className="gray">#438</small>
             </p>
             <h4>
-              Today topic is <b className="yellow topic">{jsonData.title}</b>
+              Today topic is{" "}
+              <b className="yellow topic">{todayTopicData.topic}</b>
             </h4>
             {isBtnClickable ? (
               <h1>
-                {count}
+                {count.toFixed(1)}
                 <span>s left</span>
               </h1>
             ) : (
               <h2 style={{ color: "#FC7300", fontWeight: 600 }}>Game over</h2>
             )}
           </Row>
-          {jsonData.items ? (
-            <Row className="foods mb-5">
-              {Object.keys(jsonData.items)
+          {todayTopicData.items ? (
+            <Row className="foods mb-3">
+              {Object.keys(todayTopicData.items)
                 .slice(0, visibleItems)
                 .map((item, index) => (
                   <div
@@ -222,14 +185,16 @@ const Home = () => {
                     key={item}
                   >
                     <div className="food-item col-8">
-                      <p>{jsonData.items[item].title}</p>
+                      <p>{todayTopicData.items[item]}</p>
                     </div>
                     <div
                       className="input-num col-2"
                       contentEditable={false}
                       // onKeyDown={(e) => handleKeyDown(e, index)}
                       ref={(el) => (inputRefs.current[index] = el)}
-                      dangerouslySetInnerHTML={{ __html: values[index] }}
+                      dangerouslySetInnerHTML={{
+                        __html: todayTopicData.values[index],
+                      }}
                     />
                   </div>
                 ))}
@@ -242,7 +207,7 @@ const Home = () => {
               <button
                 key={value}
                 className="keyboard-item"
-                onClick={handleKeyBoardPress}
+                onClick={(e) => handleKeyBoardPress(e)}
                 value={value}
                 disabled={!isBtnClickable}
               >
@@ -251,6 +216,47 @@ const Home = () => {
             ))}
           </Row>
         </div>
+
+        <Modal
+          show={modalShow}
+          onHide={handleClose}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+          style={{ textAlign: "center" }}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Show your result
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h2>I have jsust completed Blindly</h2>
+            <h4>Topic: {todayTopicData.topic}</h4>
+            <h4>In {(90 - count).toFixed(1)} second</h4>
+            <ol
+              className="col-xl-6 offset-xl-3 col-md-8 offset-md-2 col-sm-12"
+              style={{
+                textAlign: "justify",
+                margin: "auto",
+                fontSize: 22,
+                wordBreak: "break-all",
+                marginBottom: 20,
+              }}
+            >
+              {rankedItems.map((item) => <li>{item}</li>)}
+            </ol>
+            <div className="col-4 offset-4">
+              <ShareButton
+                compact
+                socialMedia={"twitter"}
+                url={"https://maaz-net.netlify.app"}
+                text={"I have jsust completed Blindly" + "Topic:" + todayTopicData.topic + "In " + (90 - count).toFixed(1) + " second"}
+                hashtags={"Blindlee"}
+              />
+            </div>
+          </Modal.Body>
+        </Modal>
       </Container>
     </section>
   );
